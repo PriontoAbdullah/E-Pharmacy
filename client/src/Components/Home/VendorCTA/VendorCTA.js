ecb4dfecb5df25a5bcb398173d520e1716a0a96c
Shourov Saha <shourovys@gmail.com>
2021-09-06 18:42:32 +0600

    Home>vandor section added

--- client/src/Components/Home/VendorCTA/VendorCTA.js
+++ client/src/Components/Home/VendorCTA/VendorCTA.js
@@ -1,23 +1,23 @@
 import React from "react";
 
 const VendorCTA = () => {
-
   return (
     <div>
-      <section className="px-0 py-12 mx-auto max-w-7xl sm:px-4">
+      <section className="px-0 py-12 mx-auto max-w-7xl sm:px-4 md:px-10">
         <div className="grid items-center justify-center grid-cols-1 gap-10 px-4 py-6 overflow-hidden rounded-none card card-body sm:rounded-lg md:px-10 md:grid-cols-12">
           <div className="col-span-1 md:col-span-7">
             <h2 className="mb-3 text-2xl font-normal leading-tight lg:text-4xl">
-              Sleep peacefully knowing that your website is performing at it’s
-              best.
+              Add your local pharmacy online, NOW
             </h2>
-            <ul className='list-disc'>
-              <li className='ml-4'>Lorem ipsum dolor sit adipisicing elit</li>
-              <li className='ml-4'>Lorem ipsum sit amet  adipisicing</li>
-              <li className='ml-4'>Lorem ipsum dolor adipisicing elit</li>
-              <li className='ml-4'>Lorem ipsum dolor sit adipisicing elit</li>
+            <ul className="list-disc pb-3 ml-4 text-sm text-gray-600 tracking-wide sm:text-base md:max-w-xl md:text-lg xl:text-xl">
+              <li className="">Take user orders form online</li>
+              <li className="">Create your shop profile</li>
+              <li className="">Manage your store</li>
+              <li className="">Get more orders</li>
             </ul>
-            
+            <button type="button" className="hero-search-button ">
+              Add Now
+            </button>
           </div>
           <div className="col-span-1 md:col-span-5">
             <img
