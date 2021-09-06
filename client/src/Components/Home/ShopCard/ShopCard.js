f558662d5060c5274e14d7534ef4c7b0d5a29697
amir <iamamirmostafa@gmail.com>
2021-09-06 11:52:29 +0600

    ignored lock

--- client/src/Components/Home/ShopCard/ShopCard.js
+++ client/src/Components/Home/ShopCard/ShopCard.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import shopCard from '../../../Data/vendors'
-
+import './ShopCard.css'
 
 const ShopCard = () => {
     const [shopCards, setShopCards] = useState(shopCard)
@@ -56,7 +56,7 @@ const ShopCard = () => {
                                 
                             </div>
                             <div class="flex justify-center my-2 ">
-                                <button class="flex border py-1.5 px-5 rounded shadow-lg ">
+                                <button class="flex py-1.5 px-5 rounded shadow-lg bg-teal-500 focus:outline-none active:bg-teal-500 text-white transition duration-150 ease-in-out hover:bg-teal-accent-700">
                                     <svg class="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
                                     Visit Store
                                 </button>
