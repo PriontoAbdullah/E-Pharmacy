44a578f924faa1f24b0798d579e71a0dd7e6352a
Shourov Saha <shourovys@gmail.com>
2021-09-06 13:27:29 +0600

    vendor, product, product reviews, offers fake data

---
+++ client/src/Data/ProductReviews.js
@@ -0,0 +1,62 @@
+const productReviewsData = [{
+    "_id": "8604f99a-84e4-41d2-92fc-e6d08ff623ce",
+    "userId": "c3ac7701-bcba-4102-86d7-d00d27fb9b00",
+    "productId": "e8f94f7b-6f84-41d2-999a-c3ecd32d08d8",
+    "rating": 4,
+    "comment": "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero."
+  }, {
+    "_id": "7e7fcac9-8172-40f0-b488-cd8415d0b509",
+    "userId": "80b28f09-421f-4672-bbbb-0ec0e4fd83fa",
+    "productId": "7c0e8b3e-97e2-4573-b5dc-4dfdfaeb5c80",
+    "rating": 97,
+    "comment": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo."
+  }, {
+    "_id": "967d9153-e3a6-4ba8-8ab9-031d25489a67",
+    "userId": "28ade75d-76b2-4bb4-8125-2f0121bd1b5f",
+    "productId": "7b2203f6-67e1-4ca2-91b9-20f7a146de92",
+    "rating": 91,
+    "comment": "Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.\n\nFusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.\n\nSed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus."
+  }, {
+    "_id": "978ad9b1-8786-4800-a9d3-dbcc0277d0c4",
+    "userId": "4f900e99-f8ab-45e6-98ae-22453a2e3ad1",
+    "productId": "524136c5-f2d5-4254-b613-1b2749c29e56",
+    "rating": 4,
+    "comment": "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque."
+  }, {
+    "_id": "cd68237b-869c-4bdf-98ae-59d3a42e880b",
+    "userId": "31c9b0ee-9812-4c18-902c-cde24fd7e9b4",
+    "productId": "0524a5e9-97a5-45b0-9eec-37a269b11ff1",
+    "rating": 84,
+    "comment": "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem."
+  }, {
+    "_id": "276eabee-159a-47da-8c74-f2a1be4a397a",
+    "userId": "866e1724-2141-471f-9086-a6e1d3b9ab18",
+    "productId": "f335a73e-97e6-4ceb-8a77-b1b505243361",
+    "rating": 55,
+    "comment": "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem."
+  }, {
+    "_id": "38489176-0966-456a-94ef-40bd91cc64a1",
+    "userId": "3e28185a-f64b-448a-859c-2829e7191bc9",
+    "productId": "f885cb75-6a64-49a1-8b19-587e57b38015",
+    "rating": 89,
+    "comment": "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque."
+  }, {
+    "_id": "defeb811-ffc7-42f8-b0e9-3344ece51292",
+    "userId": "2b54319a-42fd-4d7d-a95c-108a36a6cbb3",
+    "productId": "ed1643da-ad6a-43f2-bb36-d7f4f9d7bc72",
+    "rating": 85,
+    "comment": "In congue. Etiam justo. Etiam pretium iaculis justo."
+  }, {
+    "_id": "1c56ff5f-e62b-4f17-b3e2-069e96058642",
+    "userId": "d5744693-e5d4-4abb-b946-2a9fbd8821a5",
+    "productId": "351a073b-3a1a-42a4-8877-0efba5b91abe",
+    "rating": 27,
+    "comment": "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui."
+  }, {
+    "_id": "4063f372-b084-405d-a639-777736e7bef8",
+    "userId": "dacc3db2-bdf3-43e1-aa54-eb7fd4775ffa",
+    "productId": "2be8a8a1-f20a-4903-a28b-05d03a2d55f3",
+    "rating": 2,
+    "comment": "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus."
+  }]
+  export default productReviewsData
