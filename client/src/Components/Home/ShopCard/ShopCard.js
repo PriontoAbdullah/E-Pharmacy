d2b50cb8d2823897c34e43b51d4f8b06c0748cbf
amir <iamamirmostafa@gmail.com>
2021-09-07 19:05:56 +0600

    update shop card design

--- client/src/Components/Home/ShopCard/ShopCard.js
+++ client/src/Components/Home/ShopCard/ShopCard.js
@@ -9,9 +9,9 @@ const ShopCard = () => {
 
     return (
         <div>
-            
+            {/* Shop Card */}
             <div class="w-full text-center py-10">
-                <h1 class="font-bold text-3xl">Shop By Store</h1>
+                <h1 class="font-bold text-3xl font-display">Shop By Store</h1>
             </div>
             <div className="cards flex flex-wrap justify-center">
                 
@@ -19,30 +19,24 @@ const ShopCard = () => {
                     shopCards.map(shopCard => (
                         
 
-                        <div styles={{ 
-                            backgroundImage: `url(${shopCard.cardBanner})`,
-                            backgroundPosition: 'center center',
-                            backgroundSize: 'cover',
-                            backgroundRepeat: 'no-repeat'
-                            }} class="mx-3 my-8 rounded-lg shadow-xl  p-4 w-96 md:w-2/5 lg:w-1/4 ">
+                        <div class="pb-4 m-3 rounded-lg shadow-xl w-96 md:w-2/5 lg:w-1/4 bg-blue-200">
 
-                            
+                                        
+                            <img src={shopCard.cardBanner} class="rounded-t-lg" alt="" />
 
-                            
-                            <img src={shopCard.cardBanner} alt="" />
                             <div class="relative pt-2">
 
                                 <div class="absolute -mt-12 w-full flex justify-center items-center">
-                                    <img class="shadow-2xl rounded-3xl border border-2-black h-16 w-16" src={shopCard.logo} alt="" />
+                                    <img class="shadow-3xl rounded-2xl border-2 h-16 w-16" src={shopCard.logo} alt="" />
                                 </div>
 
-                                <div class="pt-5">
-                                    <div class="absolute right-0 top-2 shadow">
+                                <div class="pt-5 px-4">
+                                    <div class="absolute right-4 top-8 shadow">
                                         <p class="rounded border py-0.5 px-3">{shopCard.status}</p>
                                     </div>
 
-                                    <h1 class="font-bold text-xl pb-1.5">{shopCard.shopName}</h1>
-                                    <div class="text-sm">
+                                    <h1 class="font-bold text-xl pb-1.5 text- font-display">{shopCard.shopName}</h1>
+                                    <div class="text-sm font-sans">
                                         <p class="pb-1">{shopCard.rating}⭐</p>
                                         <p>{shopCard.location}  {shopCard['postal code']}</p>
                                         <p>{shopCard.city}</p>
@@ -57,8 +51,8 @@ const ShopCard = () => {
 
                                 
                             </div>
-                            <div class="flex justify-center my-2 ">
-                                <button class="flex py-1.5 px-5 rounded shadow-lg bg-teal-500 focus:outline-none active:bg-teal-500 text-white transition duration-150 ease-in-out hover:bg-teal-accent-700">
+                            <div class="flex justify-center my-2 mt-4 ">
+                                <button class="flex py-2 px-6 rounded shadow-lg bg-teal-500 focus:outline-none active:bg-teal-500 text-white transition duration-150 ease-in-out hover:bg-teal-accent-700">
                                     <svg class="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
                                     Visit Store
                                 </button>
